c0cd637a0879e1c0c7a3eee4bc9de532e03def7e
Sidney Nimako <snibo13@gmail.com>
2024-10-28 20:57:04 -0400

    With training

---
+++ 8 PPO.py
@@ -0,0 +1,29 @@
+import torch
+import gymnasium as gym
+from stable_baselines3 import PPO  # Import the PPO algorithm
+from stable_baselines3.common.env_util import (
+    make_vec_env,
+)  # Import the environment utility function
+from gymnasium.wrappers.filter_observation import FilterObservation
+from gymnasium.wrappers.flatten_observation import FlattenObservation
+import signal
+
+model = PPO.load("PPO-DoublePendulum")
+
+
+def make_env_human():
+    # Create the environment
+    env = gym.make("InvertedDoublePendulum-v4", render_mode="human")
+    env = FlattenObservation(env)
+    # Create the vectorized environment
+    return env
+
+
+env = make_vec_env(make_env_human)
+obs = env.reset()
+done = False
+while not done:
+    action, _states = model.predict(obs)
+    obs, rewards, done, info = env.step(action)
+    env.render()
+env.close()
